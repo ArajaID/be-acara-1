6f229330ef7abdfda85a79ebee9dcc9236cf6d6b
ArajaID <arajaid@gmail.com>
2025-01-28 14:04:06 +0700

    add api documentation

---
+++ src/docs/swagger.ts
@@ -0,0 +1,39 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+    info: {
+        version: "v0.0.1",
+        title: "Dokumentasi API Acara",
+        description: "Dokumentasi API Acara",
+    },
+    servers: [
+        {
+            url: "http://localhost:3000/api",
+            description: "Development server",
+        },
+        {
+            url: "https://back-end-acara-rosy.vercel.app/api",
+            description: "Production server",
+        }
+    ], 
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: "http",
+                scheme: "bearer",
+
+            }
+        },
+        schemas: {
+            LoginRequest: { 
+                identifier: "admin@gmail.com",
+                password: "admin123",
+            },
+        }
+    },
+}
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"];
+
+swaggerAutogen({ openapi: "3.0.0"})(outputFile, endpointsFiles, doc);
